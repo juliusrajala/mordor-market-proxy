7dbbc042b26bf7fa29f1da3b3ec8dfedd4c559d7
Julius Rajala <julius.rajala@futurice.com>
2017-01-31 21:26:47 +0200

    Basic handler skeleton for api routes.

--- src/mordor_market_rest/handler.clj
+++ src/mordor_market_rest/handler.clj
@@ -8,6 +8,19 @@
 (defn root-function []
   (content-type (response "Testing root route.") "text/html"))
 
+; GET handler functions
+
+(defn get-user [id] 
+  (content-type (response "Users data of id") "text/html")) ; TODO: Get users ID from request.
+
+(defn get-product [id]
+  (content-type (response "Product of id") "text/html")) ; TODO: Get item id from request.
+
+(defn get-empty [type]
+  (content-type (response "No id of TYPE specified"))) ; TODO: String work here.
+
+; POST handler functions
+
 (def response-object 
   {:something "Test string thing yeah."
    :else "Not my string"})
@@ -15,10 +28,19 @@
 (defn get-response []
   (content-type (response ( .toString response-object)) "text/html"))
 
+; Router function
+
 (defroutes app-routes
   (GET "/" [] (root-function))
-  (context "/test" [] (defroutes api-routes
+  (context "/test" [] (defroutes test-routes
     (GET "/" [] (get-response))))
+  (context "/user" [] (defroutes user-routes
+    (GET "/" [] (get-empty))))
+  (context "/product" [] (defroutes product-routes
+    (GET "/" [] (get-empty))))
+  (context "/purchase" [] (defroutes purchase-routes
+    (GET "/" [] (get-empty))))
+  ;TODO: POST route for purchases with items in body.
   (route/not-found "Not Found"))
 
 (def app
