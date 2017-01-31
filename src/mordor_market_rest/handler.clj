bd5cecc94231a01b8264713e040c5c2bc8f3d569
Julius Rajala <julius.rajala@futurice.com>
2017-01-31 23:47:01 +0200

    Rest-api skeleton now in place.

--- src/mordor_market_rest/handler.clj
+++ src/mordor_market_rest/handler.clj
@@ -14,13 +14,13 @@
 ; GET handler functions
 
 (defn get-user [id] 
-  (content-type (response "Users data of id") "text/html")) ; TODO: Get users ID from request.
+  (content-type (response (str "User with id " id)) "text/html")) ; TODO: Get users ID from request.
 
 (defn get-product [id]
-  (content-type (response "Product of id") "text/html")) ; TODO: Get item id from request.
+  (content-type (response (str "Product with id " id)) "text/html")) ; TODO: Get item id from request.
 
 (defn get-empty [type]
-  (content-type (response (clojure.string/join ["No id of " type " specified"])))) ; TODO: String work here.
+  (content-type (response (str "No id of " type " specified")) "text/html")) ; TODO: String work here.
 
 ; POST handler functions
 
@@ -38,9 +38,11 @@
   (context "/test" [] (defroutes test-routes
     (GET "/" [] (get-response))))
   (context "/user" [] (defroutes user-routes
-    (GET "/" [] (get-empty "USER"))))
+    (GET "/" [] (get-empty "USER"))
+    (GET "/:id" [id] (get-user id))))
   (context "/product" [] (defroutes product-routes
-    (GET "/" [] (get-empty "PRODUCT"))))
+    (GET "/" [] (get-empty "PRODUCT"))
+    (GET "/:id" [id] (get-product id))))
   (context "/purchase" [] (defroutes purchase-routes
     (GET "/" [] (get-empty "PURCHASE"))))
   ;TODO: POST route for purchases with items in body.
